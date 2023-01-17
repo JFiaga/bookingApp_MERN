f84b5477d7b4da843951862593a9ca387efc4013
Fiaga Fils <fiagafils@gmail.com>
2023-01-17 03:22:34 +0100

    end of the searchtools

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -5,17 +5,13 @@ import "react-date-range/dist/theme/default.css";
 import { DateRange } from "react-date-range";
 import { format } from "date-fns";
 import { FaBed, FaCalendar, FaCar, FaFly, FaTaxi } from "react-icons/fa";
-import { AiOutlineLineChart } from "react-icons/ai";
+import {
+  AiFillCloseCircle,
+  AiFillTool,
+  AiOutlineLineChart,
+} from "react-icons/ai";
 
 const Header = () => {
-  const [calenderOn, setCalenderOn] = useState(false);
-  const [date, setDate] = useState([
-    {
-      startDate: new Date(),
-      endDate: new Date(),
-      key: "selection",
-    },
-  ]);
   const headerIconsValues = [
     { icon: FaBed, value: "stays" },
     { icon: FaFly, value: "flights" },
@@ -24,8 +20,33 @@ const Header = () => {
     { icon: FaTaxi, value: "airport taxis" },
   ];
 
+  const [calenderOn, setCalenderOn] = useState(false);
+  const [date, setDate] = useState([
+    {
+      startDate: new Date(),
+      endDate: new Date(),
+      key: "selection",
+    },
+  ]);
+  const [options, setOptions] = useState({
+    adults: 0,
+    children: 0,
+    rooms: 0,
+  });
+  const handleOptions = (name, operations) => {
+    setOptions((prev) => {
+      return {
+        ...prev,
+        [name]: operations === "i" ? options[name] + 1 : options[name] - 1,
+      };
+    });
+  };
+
+  const [optionsOn, setOptionsOn] = useState(false);
   return (
-    <header>
+    <header
+  
+    > 
       <div className="icons__container">
         {headerIconsValues.map((item, index) => (
           <div key={index} className="icons__container--box">
@@ -55,16 +76,75 @@ const Header = () => {
             )}`}
             onClick={() => setCalenderOn((bool) => !bool)}
           />
-          {calenderOn &&  <DateRange
-            editableDateInputs={false}
-            onChange={(item) => setDate([item.selection])}
-            moveRangeOnFirstSelection={false}
-            ranges={date}
-            className='datePicker'
+          {calenderOn && (
+            <DateRange
+              editableDateInputs={false}
+              onChange={(item) => setDate([item.selection])}
+              moveRangeOnFirstSelection={false}
+              ranges={date}
+              className="datePicker"
             />
-          }
-          
-         
+          )}
+        </div>
+
+        <div
+          className="header__reservationBox--options"
+          onClick={() => setOptionsOn(prev => !prev)}
+        >
+          <AiFillTool />
+          <div>{`${options.children} Childrens - ${options.adults} Adults - ${options.rooms} Rooms `}</div>
+
+          <div
+          onClick={(e) => e.stopPropagation()}
+            className={`reservationBox__options--btn ${optionsOn && "enabled"}`}
+          >
+       
+            <div
+            
+            className="options__btnBox">
+              <span>adults</span>
+              <div>
+                <button
+                  disabled={options.adults === 0}
+                  onClick={() => handleOptions("adults", "d")}
+                >
+                  -
+                </button>
+                <span>{options.adults}</span>
+                <button onClick={() => handleOptions("adults", "i")}>+</button>
+              </div>
+            </div>
+
+            <div className="options__btnBox">
+              <span>children</span>
+              <div>
+                <button
+                  disabled={options.children === 0}
+                  onClick={() => handleOptions("children", "d")}
+                >
+                  -
+                </button>
+                <span>{options.children}</span>
+                <button onClick={() => handleOptions("children", "i")}>
+                  +
+                </button>
+              </div>
+            </div>
+
+            <div className="options__btnBox">
+              <span>rooms</span>
+              <div>
+                <button
+                  disabled={options.rooms === 0}
+                  onClick={() => handleOptions("rooms", "d")}
+                >
+                  -
+                </button>
+                <span>{options.rooms}</span>
+                <button onClick={() => handleOptions("rooms", "i")}>+</button>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </header>
