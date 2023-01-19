49716fd2a177c92802e4ee73bb0a040e23acdf30
Fiaga Fils <fiagafils@gmail.com>
2023-01-19 17:19:20 +0100

    add the hotels information components

--- src/pages/hotels/Hotels.jsx
+++ src/pages/hotels/Hotels.jsx
@@ -1,9 +1,39 @@
-import React from 'react'
+import { Header } from "../../components";
+import { CiLocationOn } from "react-icons/ci";
+import "./Hotels.scss";
 
 const Hotels = () => {
   return (
-    <div>Hotels</div>
-  )
-}
+    <>
+      <Header type={"hotelList"} />
+      <div className="hotelsPage">
+        <div className="hotelsPage__desc">
+          <div>
+            <h2>Grand Hotel</h2>
+            <span>
+              <CiLocationOn /> {""} Elton St 125 New York{" "}
+            </span>
+            <span>Excellent location - 500m from center</span>
+            <span>
+              Book a stay $114 at this property and get a free airport taxi
+            </span>
+          </div>
 
-export default Hotels
+          <button className="hotelsPage__desc--btn">Reserve or Book Now</button>
+        </div>
+
+        <div className="hotelsPage__imgContainer">
+          <div className="hotelsPage__imgContainer--imgBox">
+            <img
+              src="https://images.unsplash.com/photo-1659884700210-5d00b9656511?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1117&q=80"
+              alt=""
+            />
+          </div>{" "}
+
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Hotels;
--- src/pages/hotelsList/HotelsList.jsx
+++ src/pages/hotelsList/HotelsList.jsx
@@ -10,111 +10,113 @@ const HotelsList = () => {
   const location = useLocation();
 
   const [date, setDate] = useState(location.state.date);
-  const [calenderOn, setCalenderOn] = useState(true);
+  const [calenderOn, setCalenderOn] = useState(false);
   const [options, setOptions] = useState(location.state.options);
   const [destination, setDestination] = useState(location.state.destination);
 
   return (
     <>
       <Header type={"hotelList"} />
-      <div className="hotelList__container">
+    <div className="hotelList">
+    <div className="hotelList__container">
 
 
-        <div className="hotelList__container--searchBox">
-          <span className="search">search</span>
-          <div className="searchBox__destination">
-            <label htmlFor="destination">Destination</label>
-            <input
-              onChange={(e) => setDestination(e.target.value)}
-              type="text"
-              id="destination"
-              defaultValue={destination || "Madrid"}
-              placeholder="Enter your destination"
-            />
-          </div>
+<div className="hotelList__container--searchBox">
+  <span className="search">search</span>
+  <div className="searchBox__destination">
+    <label htmlFor="destination">Destination</label>
+    <input
+      onChange={(e) => setDestination(e.target.value)}
+      type="text"
+      id="destination"
+      defaultValue={destination || "Madrid"}
+      placeholder="Enter your destination"
+    />
+  </div>
 
-          <div className="searchBox__date">
-            <label htmlFor="destination">Check-in-date</label>
-            <span onClick={() => setCalenderOn((prev) => !prev)}>
-              {`${format(date[0].startDate, "MM/dd/yy")} to ${format(
-                date[0].endDate,
-                "MM/dd/yy"
-              )}`}
-            </span>
-            {calenderOn && (
-              <div>
-                <DateRange
-                    minDate={new Date()}
-                    editableDateInputs={false}
-                    onChange={(item) => setDate([item.selection])}
-                    moveRangeOnFirstSelection={false}
-                    ranges={date}
-                    className="datePicker"
-                />
-              </div>
-            )}
-          </div>
-
-          <div className="searchBox__options">
-            <span>Options</span>
-            <div>
-              <label htmlFor="minPrice">Min Price</label>
-              <input type="number" max="99" min="0" id="minPrice" />
-            </div>
-            <div>
-              <label htmlFor="maxPrice">Max Price</label>
-              <input type="number" max={99} min={0} id="maxPrice" />
-            </div>
-            <div>
-              <label htmlFor="adult">adults</label>
-              <input
-                type="number"
-                max={99}
-                min={0}
-                id="adult"
-                defaultValue={options.adults}
-              />
-            </div>
-            <div>
-              <label htmlFor="children">childrens</label>
-              <input
-                type="number"
-                max={99}
-                min={0}
-                id="children"
-                defaultValue={options.childrens}
-              />
-            </div>{" "}
-            <div>
-              <label htmlFor="rooms">rooms</label>
-              <input
-                type="number"
-                max={99}
-                min={0}
-                id="rooms"
-                defaultValue={options.rooms}
-              />
-            </div>
-
-          </div>
-          <button className="searchBox__btn">Search</button>
-        
-        </div>
-
-        {/* Results */}
-        <div className="hotelList__container--results">
-          <HotelsListResults/>
-          <HotelsListResults/>
-          <HotelsListResults/>
-          <HotelsListResults/>
-          <HotelsListResults/>
-          <HotelsListResults/>
-          <HotelsListResults/>
-          <HotelsListResults/>
-          <HotelsListResults/>
-          <HotelsListResults/>
-        </div>
+  <div className="searchBox__date">
+    <label htmlFor="destination">Check-in-date</label>
+    <span onClick={() => setCalenderOn((prev) => !prev)}>
+      {`${format(date[0].startDate, "MM/dd/yy")} to ${format(
+        date[0].endDate,
+        "MM/dd/yy"
+      )}`}
+    </span>
+    {calenderOn && (
+      <div>
+        <DateRange
+            minDate={new Date()}
+            editableDateInputs={false}
+            onChange={(item) => setDate([item.selection])}
+            moveRangeOnFirstSelection={false}
+            ranges={date}
+            className="datePicker"
+        />
       </div>
+    )}
+  </div>
+
+  <div className="searchBox__options">
+    <span>Options</span>
+    <div>
+      <label htmlFor="minPrice">Min Price</label>
+      <input type="number" max="99" min="0" id="minPrice" />
+    </div>
+    <div>
+      <label htmlFor="maxPrice">Max Price</label>
+      <input type="number" max={99} min={0} id="maxPrice" />
+    </div>
+    <div>
+      <label htmlFor="adult">adults</label>
+      <input
+        type="number"
+        max={99}
+        min={0}
+        id="adult"
+        defaultValue={options.adults}
+      />
+    </div>
+    <div>
+      <label htmlFor="children">childrens</label>
+      <input
+        type="number"
+        max={99}
+        min={0}
+        id="children"
+        defaultValue={options.childrens}
+      />
+    </div>{" "}
+    <div>
+      <label htmlFor="rooms">rooms</label>
+      <input
+        type="number"
+        max={99}
+        min={0}
+        id="rooms"
+        defaultValue={options.rooms}
+      />
+    </div>
+
+  </div>
+  <button className="searchBox__btn">Search</button>
+
+</div>
+
+{/* Results */}
+<div className="hotelList__container--results">
+  <HotelsListResults/>
+  <HotelsListResults/>
+  <HotelsListResults/>
+  <HotelsListResults/>
+  <HotelsListResults/>
+  <HotelsListResults/>
+  <HotelsListResults/>
+  <HotelsListResults/>
+  <HotelsListResults/>
+  <HotelsListResults/>
+</div>
+</div>
+    </div>
     </>
   );
 };
