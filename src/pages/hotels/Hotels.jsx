00c63791543fd59b03dfc92b842c7772e4507532
Fiaga Fils <fiagafils@gmail.com>
2023-01-19 20:05:10 +0100

    Add the carousel

--- src/pages/hotels/Hotels.jsx
+++ src/pages/hotels/Hotels.jsx
@@ -2,9 +2,34 @@ import { Footer, Header, Contact } from "../../components";
 import { CiLocationOn } from "react-icons/ci";
 import { hotelsDetailImgArr } from "../../constant";
 import "./Hotels.scss";
+import { useState } from "react";
+import { FaArrowAltCircleLeft, FaArrowAltCircleRight } from "react-icons/fa";
 
 
 const Hotels = () => {
+  const [openImg, setOpenImg] = useState()
+const [indexImg, setindexImg] = useState(1)
+
+const handleRightClickImg = ()=>{
+    if(indexImg<5){
+      setindexImg(indexImg + 1)
+      console.log(indexImg);
+    }
+    else{
+      setindexImg(0)
+      console.log(indexImg);
+    }
+}
+const handleLeftClickImg =() => {
+  if(indexImg ===0){
+    setindexImg(5)
+    console.log(indexImg);
+  }
+  else {
+    setindexImg( indexImg -1)
+    console.log(indexImg);
+  }
+}
   return (
     <>
       <Header type={"hotelList"} />
@@ -13,8 +38,7 @@ const Hotels = () => {
           <div>
             <h2>Grand Hotel</h2>
             <span>
-              <CiLocationOn />  Elton St 125 New York
-
+              <CiLocationOn /> Elton St 125 New York
             </span>
             <span>Excellent location - 500m from center</span>
             <span>
@@ -26,22 +50,51 @@ const Hotels = () => {
         </div>
 
         <div className="hotelsPage__imgContainer">
-        {hotelsDetailImgArr.map((imgSrc, index)=> (
-
-          <div
-          key={index}
-          className="hotelsPage__imgContainer--imgBox">
-            <img
-              src={imgSrc.imgSrc}
-              alt=""
-              />
-          </div>
-              ))}
+          {hotelsDetailImgArr.map((imgSrc, index) => (
+            <div
+              onClick={() => setOpenImg((prev) => !prev)}
+              key={index}
+              className="hotelsPage__imgContainer--imgBox"
+            >
+              <img
+              onClick={() => setindexImg(index)}
+              src={imgSrc.imgSrc} alt="" />
+            </div>
+          ))}
         </div>
+
+        <div
+          onClick={() => setOpenImg(() => false)}
+          className={`hotelsPages__caroussel ${!openImg && "none"}`}
+        >
+          <img
+            onClick={(e) => e.stopPropagation()}
+            src={hotelsDetailImgArr[indexImg]?.imgSrc}
+            alt=""
+          />
+
+<div  className=" hotelsPages__caroussel--arrow">
+          <FaArrowAltCircleLeft 
+             onClick={(e) => {
+              e.stopPropagation()
+              handleLeftClickImg()
+            }}
+          className="leftArrow"/>
+          <FaArrowAltCircleRight 
+           onClick={(e) => {
+            e.stopPropagation()
+            handleRightClickImg()
+          }
+          }
+          className="rightArrow"/>
+        </div>
+        </div>
+
+       
       </div>
 
-        <Contact/>
-        <Footer/>
+      <Contact />
+      <Footer />
     </>
   );
 };
